52f532a0f37fe60fe960c56c584689d7827e7fda
Imogen Campbell <imogen.campbell@cef.co.uk>
2022-12-22 12:56:08 +0000

    NATACC-751 Add YARDocs

--- lib/cxml/from.rb
+++ lib/cxml/from.rb
@@ -5,6 +5,9 @@ module CXML
   class From
     attr_accessor :credential, :user_agent
 
+    # On initialize, we build the Credentials and the UserAgent.
+    # This is called when ingesting a CXML object and when building one.
+    # @param data [Hash] The From data for the CXML object that's being handled.
     def initialize(data = {})
       return unless data.is_a?(Hash) || data.empty?
 
@@ -12,6 +15,9 @@ module CXML
       @user_agent = data['UserAgent']
     end
 
+    # This is only called when building a CXML object, not when ingesting one.
+    # @param node [Nokogiri::XML::Builder] The XML object being built in Nokogiri.
+    # @return [Nokogiri::XML::Builder] The XML object, with added From attribute.
     def render(node)
       node.From do |n|
         @credential.render(n)
--- lib/cxml/sender.rb
+++ lib/cxml/sender.rb
@@ -8,6 +8,9 @@ module CXML
   class Sender
     attr_accessor :credential, :user_agent
 
+    # On initialize, we build the Credentials and the UserAgent.
+    # This is called when ingesting a CXML object and when building one.
+    # @param data [Hash] The Sender data for the CXML object that's being handled.
     def initialize(data = {})
       return unless data.is_a?(Hash) || data.empty?
 
@@ -15,6 +18,9 @@ module CXML
       @user_agent = data['UserAgent']
     end
 
+    # This is only called when building a CXML object, not when ingesting one.
+    # @param node [Nokogiri::XML::Builder] The XML object being built in Nokogiri.
+    # @return [Nokogiri::XML::Builder] The XML object, with added Sender attribute.
     def render(node)
       node.Sender do |n|
         @credential.render(n)
