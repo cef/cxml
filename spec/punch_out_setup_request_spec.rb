6a0f801693dbd3c6320a01544d3913ba19787435
Carine Vunabandi <carine.vunabandi@cef.co.uk>
2025-12-08 12:35:03 +0000

    NATACC-1669: Make the SupplierSetup block from cXML requests optional, refactor and update spec

--- spec/punch_out_setup_request_spec.rb
+++ spec/punch_out_setup_request_spec.rb
@@ -2,22 +2,34 @@ require 'spec_helper'
 
 describe CXML::PunchOutSetupRequest do
 
-  it { should respond_to :browser_form_post_url }
-  it { should respond_to :supplier_setup_url }
-
-
-  let(:parser) { CXML::Parser.new }
-  let(:data) { parser.parse(fixture('punch_out_setup_request_doc.xml')) }
-  let(:doc) { CXML::Document.new(data) }
-  let(:request) { doc.request }
-  let(:punch_out_setup_request) { request.punch_out_setup_request }
-
+  it { is_expected.to respond_to(:browser_form_post_url) }
+  it { is_expected.to respond_to(:supplier_setup_url) }
 
   describe '#initialize' do
-    it "sets the mandatory attributes" do
-      punch_out_setup_request.browser_form_post_url.should_not be_nil
-      punch_out_setup_request.supplier_setup_url.should_not be_nil
+    context "when the request data comes with a SupplierSetup block" do
+      let(:parser)  { CXML::Parser.new }
+      let(:data)    { parser.parse(fixture('punch_out_setup_request_doc.xml')) }
+      let(:doc)     { CXML::Document.new(data) }
+      let(:request) { doc.request }
+      let(:punch_out_setup_request) { request.punch_out_setup_request }
+
+      it "sets the mandatory attributes including the SupplierSetup" do
+        expect(punch_out_setup_request.browser_form_post_url).not_to be_nil
+        expect(punch_out_setup_request.supplier_setup_url).not_to be_nil
+      end
+    end
+
+    context "when the request data comes without a SupplierSetup block" do
+      let(:parser)  { CXML::Parser.new }
+      let(:data)    { parser.parse(fixture('punch_out_setup_request_doc_without_supplier.xml')) }
+      let(:doc)     { CXML::Document.new(data) }
+      let(:request) { doc.request }
+      let(:punch_out_setup_request) { request.punch_out_setup_request }
+
+      it "sets the mandatory attributes without the SupplierSetup" do
+        expect(punch_out_setup_request.browser_form_post_url).not_to be_nil
+        expect(punch_out_setup_request.supplier_setup_url).to be_nil
+      end
     end
   end
-
 end
